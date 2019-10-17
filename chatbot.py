7210cbde5420478096b7f5e4c08a3d22b044d313
Unknown <jcary52@live.co.uk>
2019-10-17 16:45:00 +0800

    Upload

---
+++ chatbot.py
@@ -0,0 +1,214 @@
+# building a chatbot with deep NLP
+
+# import libraries
+import numpy as np
+import tensorflow as tf
+import re 
+import time
+
+###### data processing  ######
+
+# import dataset
+lines = open('movie_lines.txt').read().split('\n')
+conversations = open('movie_conversations.txt').read().split('\n')
+
+# create dictionary mapping each line with id
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+# create dictionary for conversations ids
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversations_ids.append(_conversation.split(","))
+
+# get questions and answers seperately
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+        
+# first cleaning of text
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"there's", "there is", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"can ' t", "cannot", text)
+    text = re.sub(r"didn't", "did not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"doesn't", "does not", text)
+    text = re.sub(r"aren't", "are not", text)
+    text = re.sub(r"isn't", "is not", text)
+    text = re.sub(r"haven't", "have not", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"[^a-zA-Z\d\s:]", "", text)
+
+    return text
+
+# cleaning questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+
+# cleaning answers:
+clean_answers = []
+for answer in answers:
+    clean_answers.append(clean_text(answer))
+    
+# create dictionary that maps each word to its number of occurences
+word2count = {}
+for question in clean_questions:
+    for word in question.split(" "):
+        if word not in word2count:
+            word2count[word] = 1
+        else: 
+            word2count[word] += 1
+
+for answer in clean_answers:
+    for word in answer.split(" "):
+        if word not in word2count:
+            word2count[word] = 1
+        else: 
+            word2count[word] += 1        
+        
+# create two dictionaries to map answer words and question words to a unique integer
+threshold = 20
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number += 1
+   
+answerwords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        answerwords2int[word] = word_number
+        word_number += 1
+
+# add last tokens to dictionaries
+tokens = ['<PAD>','<EOS>','<OUT>','<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+    answerwords2int[token] = len(answerwords2int) + 1        
+
+# create inverse answerwords2int dictionary
+answersint2words = {w_i: w for w, w_i in answerwords2int.items()}
+
+# add end of string to each answers
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+        
+# transform words in question to accordance int
+questions_to_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_to_int.append(ints)
+    
+# transform words in answer to accordance int
+answers_to_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerwords2int:
+            ints.append(answerwords2int['<OUT>'])
+        else:
+            ints.append(answerwords2int[word])
+    answers_to_int.append(ints)
+    
+# sort questions and answers by length
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
+    for i in enumerate(questions_to_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_to_int[i[0]])
+            sorted_clean_answers.append(answers_to_int[i[0]])
+        
+        
+########## build seq2seq2 model #############
+
+# create placeholder for input and target
+def model_inputs():
+    inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
+    targets = tf.placeholder(tf.int32, [None, None], name = 'target')
+    lr = tf.placeholder(tf.float32, name = 'learning rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return inputs, targets, lr, keep_prob
+
+# processing target
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], wors2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side],1)
+    return preprocessed_targets
+
+# create encoder RNN layer
+def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+    encoder_cell = tf.contrib.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell,
+                                                       cell_bw = encoder_cell,
+                                                       sequence_length = sequence_length,
+                                                       inputs = rnn_inputs,
+                                                       dtype = tf.float32)
+    return encoder_state
+
+# decoding training set
+def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, decoding_scope, output_function, keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
+    training_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],
+                                                                              attention_keys,
+                                                                              attention_values,
+                                                                              attention_score_function,
+                                                                              attention_construct_function,
+                                                                              name = "attn_dec_train")
+    decoder_output, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                                                              training_decoder_function,
+                                                                                                              decoder_embedded_input,
+                                                                                                              sequence_length,
+                                                                                                              scope = decoding_scope)
+    decoder_output_dropout = tf.nn.dropout(decoder_output, keep_prob)
+    return output_function(decoder_output_dropout)
+
+######### training seq2seq model ###########
+epochs = 100
+batch_size = 64
+rnn_size = 512
+num_layer = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learning_rate = 0.01
+learning_rate_decay = 0.9
+min_learning_rate = 0.0001
+keep_probability = 0.5
+
+
+
+    
+            
